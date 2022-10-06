df175b69d60c7fd2f73599c3ca0a9aa752b92673
Abhijith Kannan <abhijithkannan452@gmail.com>
2022-10-06 12:56:01 +0530

    udpate 2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c0d608e93569dd74"; }
+/******/ 		__webpack_require__.h = function() { return "925f03101eb11def"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
